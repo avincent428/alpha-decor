4d6070687e68d1188d6b70f40a59e5313fa631c6
avincent428 <amielvincent@gmail.com>
2023-01-11 10:19:38 -0600

    made gallery edits

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import InfoBar from "./components/InfoBar";
 // 2. Testimonials
 // 3. Contact Page/Form
 // 4. Make Mobile-Friendly
+// 5. Add images to Gallery
+// 6. Footer
 function App() {
   return (
     <>
