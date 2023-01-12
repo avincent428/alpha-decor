6b165560fe34e906c374c7918b4de4c610289886
avincent428 <amielvincent@gmail.com>
2023-01-12 12:13:58 -0600

    fixed cake-table option error

--- src/components/GalleryOptions.js
+++ src/components/GalleryOptions.js
@@ -4,28 +4,33 @@ import "./GalleryOptions.css";
 
 const GalleryOptions = () => {
   return (
-    <div className="gallery-options">
-      <Link to="/gallery/centerpieces" className="link-styling">
-        <div className="grid-item centerpieces">Centerpieces</div>
-      </Link>
-      <Link to="/gallery/stage-decor" className="link-styling">
-        <div className="grid-item stage-decor"> Stage Decor </div>
-      </Link>
-      <Link to="/gallery/reception-decor" className="link-styling">
-        <div className="grid-item reception-decor"> Reception Decor </div>
-      </Link>
-      <Link to="/gallery/ceremony" className="link-styling">
-        <div className="grid-item ceremonies"> Ceremonies </div>
-      </Link>
-      <Link to="/gallery/bouquets" className="link-styling">
-        <div className="grid-item bouquets"> Bouquets </div>
-      </Link>
-      <Link to="/gallery/head-tables" className="link-styling">
-        <div className="grid-item head-tables"> Head Tables </div>
-      </Link>
-      <Link to="/gallery/cake-tables" className="link-styling">
-        <div className="grid-item cake-tables"> Cake Tables </div>
-      </Link>
+    <div>
+      <div className="gallery-options">
+        <Link to="/gallery/centerpieces" className="link-styling">
+          <div className="grid-item centerpieces">Centerpieces</div>
+        </Link>
+        <Link to="/gallery/stage-decor" className="link-styling">
+          <div className="grid-item stage-decor"> Stage Decor </div>
+        </Link>
+        <Link to="/gallery/reception-decor" className="link-styling">
+          <div className="grid-item reception-decor"> Reception Decor </div>
+        </Link>
+        <Link to="/gallery/ceremony" className="link-styling">
+          <div className="grid-item ceremonies"> Ceremonies </div>
+        </Link>
+        <Link to="/gallery/bouquets" className="link-styling">
+          <div className="grid-item bouquets"> Bouquets </div>
+        </Link>
+        <Link to="/gallery/head-tables" className="link-styling">
+          <div className="grid-item head-tables"> Head Tables </div>
+        </Link>
+        <Link
+          to="/gallery/cake-tables"
+          className="link-styling cake-tables-container"
+        >
+          <div className="grid-item cake-tables"> Cake Tables </div>
+        </Link>
+      </div>
     </div>
   );
 };
